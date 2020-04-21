837751a635fbaadc9025652604eef1f8af4867a6
jeagan <jeagan@MacBook-Pro-de-jeagan.local>
2020-04-21 09:34:11 +0200

    one_version_circus

---
+++ WildCircusvTest/index.php
@@ -0,0 +1,26 @@
+<?php
+	include "php/contact.php";
+?>
+<?php include_once ('html/head.html'); ?>
+<body>
+	<section>
+		<?php include "html/nav.html"; ?>
+	</section>
+	<section id="Performances">
+		<div class="divider"></div>
+		<?php include "html/performances.html"; ?>
+	</section>
+	<section id="AboutUs">
+		<div class="divider"></div>
+		<?php include "html/about.html"; ?>
+	</section>
+	<section id="Prices">
+		<div class="divider"></div>
+		<?php include "html/prices.html"; ?>
+	</section>
+	<section id="Contact">
+		<div class="divider"></div>
+		<?php include "html/contact.html"; ?>
+	</section>
+</body>
+<?php include_once ('html/foot.html'); ?>
